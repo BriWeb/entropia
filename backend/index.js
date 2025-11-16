11cc1be514496a12a5caa5d87477bf7d13a67e0b
Brian <brian_coder_herrera@hotmail.com>
2025-11-16 18:33:11 -0300

    options cors

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.options("*", cors());
+
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
